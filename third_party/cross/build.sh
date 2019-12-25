bbb96b57e6bdb3d051284ccd2f0df57c88e8a720
Figroc Chen <figroc@gmail.com>
2019-12-25 14:40:06 +0800

    replace wget with curl

--- third_party/cross/build.sh
+++ third_party/cross/build.sh
@@ -13,7 +13,7 @@ RUN yum install -y \
     yum clean all
 
 ENV SWIG_VER=rel-3.0.12
-RUN wget --progress=dot:mega \
+RUN curl -fLO \
       https://github.com/swig/swig/archive/${SWIG_VER}.tar.gz && \
     tar xzf ${SWIG_VER}.tar.gz && cd swig-${SWIG_VER} && \
     ./autogen.sh && ./configure && make && make install && \
@@ -21,7 +21,7 @@ RUN wget --progress=dot:mega \
 
 RUN for py3 in /opt/python/cp3*/bin/python3; do \
       export ver=$(${py3} --version | cut -d' ' -f2) && \
-      wget --progress=dot:mega \
+      curl -fLO \
         https://www.python.org/ftp/python/${ver/[a-z]*/}/Python-${ver}.tgz && \
       tar xzf Python-${ver}.tgz && cd Python-${ver} && \
       ./configure --enable-shared --enable-optimizations && make altinstall && \
@@ -29,7 +29,7 @@ RUN for py3 in /opt/python/cp3*/bin/python3; do \
     done
 
 ENV WHEEL_VER=wheel-0.32.3-py2.py3-none-any.whl
-RUN wget --progress=dot:mega \
+RUN curl -fLO \
       https://files.pythonhosted.org/packages/ff/47/1dfa4795e24fd6f93d5d58602dd716c3f101cfd5a77cd9acbe519b44a0a9/${WHEEL_VER} && \
     for pip in /usr/local/bin/pip3.*; do \
       ${pip} --no-cache-dir install --no-index ${WHEEL_VER} || exit 1; \
