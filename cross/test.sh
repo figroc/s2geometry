c170df4991725372333f39a2da1deb530f9b5d9f
Figroc Chen <figroc@gmail.com>
2019-12-27 13:05:09 +0800

    run full test against cross build

--- cross/test.sh
+++ cross/test.sh
@@ -4,8 +4,10 @@ cd $(dirname ${BASH_SOURCE[0]})/..
 
 for whl in ${1:-dist}/*-cp3*.whl; do
   ver=$(expr ${whl##*/} : '.*-cp3\(.\)-.*')
-  docker run --rm -v $(pwd)/${whl%/*}:/dist:ro --entrypoint bash python:3.${ver}-slim -c \
-    "pip install --no-index /dist/${whl##*/} && \
-     python -c 'from s2geometry import S2CellId, S2LatLng; \
-                print(S2CellId(S2LatLng.FromDegrees(110, 66)).ToToken());'"
+  docker run --rm \
+    -v $(pwd)/${whl%/*}:/dist:ro \
+    -v $(pwd)/src/python/pywraps2_test.py:/test.py:ro \
+    --entrypoint bash python:3.${ver}-slim -c "\
+      pip install --no-index /dist/${whl##*/} && \
+      python <(sed 's/pywraps2/s2geometry/g' test.py)"
 done
